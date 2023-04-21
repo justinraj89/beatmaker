93644c642e109cdb8a3acf82c2cf4035b3066775
Justin Rajewski <rajewskijustin@gmail.com>
2023-04-20 23:06:51 -0700

    added shadow ids to the audio button props to change each buttons shadow seperatley

--- src/App.js
+++ src/App.js
@@ -2,25 +2,32 @@ import SoundBtn from "./components/SoundBtn";
 import Footer from "./components/Footer";
 
 function App() {
+
   return (
-    <main className="flex flex-col items-center pt-20 lg:pt-40 h-screen bg-black">
-      <h1 className="text-gray-100 pb-12 lg:pb-4 text-4xl lg:text-7xl font-mono">
+    <main className="flex flex-col items-center pt-20 lg:pt-40 h-screen bg-stone-950">
+      <h1 className="text-slate-100 pb-12 lg:pb-4 text-4xl lg:text-7xl font-mono">
         make a beat 🥁
       </h1>
-      <h2 className="hidden lg:block text-gray-100 font-mono pb-16 text-2xl">
+      <h2 className="hidden lg:block text-gray-100 font-mono pb-8 text-2xl">
         Press the keys on your keyboard
       </h2>
-      <div className="flex flex-wrap items-center justify-center lg:flex gap-4 lg:gap-2">
-        <SoundBtn keyChar="a" soundFile="/sounds/boom.wav" sound="808" />
-        <SoundBtn keyChar="s" soundFile="/sounds/clap.wav" sound="clap" />
-        <SoundBtn keyChar="d" soundFile="/sounds/hihat.wav" sound="hihat" />
-        <SoundBtn keyChar="f" soundFile="/sounds/kick.wav" sound="kick" />
-        <SoundBtn keyChar="g" soundFile="/sounds/openhat.wav" sound="openhat" />
-        <SoundBtn keyChar="h" soundFile="/sounds/ride.wav" sound="ride" />
-        <SoundBtn keyChar="j" soundFile="/sounds/snare.wav" sound="snare" />
-        <SoundBtn keyChar="k" soundFile="/sounds/tink.wav" sound="tink" />
-        <SoundBtn keyChar="l" soundFile="/sounds/tom.wav" sound="tom" />
+      <div className="flex flex-wrap items-center justify-center lg:flex gap-4 lg:gap-3">
+        <SoundBtn keyChar="a" soundFile="/sounds/boom.wav" sound="808" shadowId='shadow-red-600' />
+        <SoundBtn keyChar="s" soundFile="/sounds/kick.wav" sound="kick" shadowId='shadow-blue-600' />
+        <SoundBtn keyChar="d" soundFile="/sounds/hihat.wav" sound="hihat" shadowId='shadow-green-600'/>
+        <SoundBtn keyChar="f" soundFile="/sounds/yeaho.wav" sound="🔥" shadowId='shadow-orange-600'/>
+        <SoundBtn keyChar="g" soundFile="/sounds/openhat.wav" sound="openhat" shadowId='shadow-emerald-600'/>
+        <SoundBtn keyChar="h" soundFile="/sounds/clap.wav" sound="clap" shadowId='shadow-yellow-600'/>
+        <SoundBtn keyChar="j" soundFile="/sounds/snare.wav" sound="snare" shadowId='shadow-blue-600'/>
+        <SoundBtn keyChar="k" soundFile="/sounds/tink.wav" sound="tink" shadowId='shadow-yellow-600'/>
+        <SoundBtn keyChar="l" soundFile="/sounds/tom.wav" sound="tom" shadowId='shadow-red-600'/>
       </div>
+      {/* <div className="flex flex-wrap items-center justify-center lg:flex gap-4 lg:gap-2">
+        <SoundBtn keyChar="c" soundFile="/sounds/boom.wav" sound="808" />
+        <SoundBtn keyChar="v" soundFile="/sounds/kick.wav" sound="kick" />
+        <SoundBtn keyChar="b" soundFile="/sounds/hihat.wav" sound="hihat" />
+        
+      </div> */}
       <Footer />
     </main>
   );
--- src/components/SoundBtn.js
+++ src/components/SoundBtn.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function SoundBtn({ keyChar, soundFile, sound }) {
+function SoundBtn({ keyChar, soundFile, sound, shadowId }) {
   const [audio] = useState(new Audio(soundFile));
   const [isPlaying, setIsPlaying] = useState(false);
 
@@ -11,6 +11,8 @@ function SoundBtn({ keyChar, soundFile, sound }) {
     setTimeout(() => setIsPlaying(false), 110);
   };
 
+  
+//Logic for assigning keys to the keyChar
   useEffect(() => {
     const handleKeyDown = (e) => {
       if (e.key === keyChar) {
@@ -30,19 +32,42 @@ function SoundBtn({ keyChar, soundFile, sound }) {
 
   return (
     <button
-      onClick={handleButtonClick}
-      className={`bg-gray-100 h-24 w-24 lg:h-32 lg:w-32 flex justify-center items-center rounded-lg transform transition-transform duration-150 focus:outline-none ${
-        isPlaying ? "scale-95 shadow-lg shadow-red-500 bg-red-100" : ""
-      }`}
+    onClick={handleButtonClick}
+    className={` bg-slate-100  h-24 w-24 lg:h-32 lg:w-32 flex justify-center items-center rounded-lg transform transition-transform duration-150 focus:outline-none ${
+      isPlaying ? `scale-95 shadow-lg ${shadowId} ` : ""
+    }`}
     >
-      <div className="flex flex-col">
-        <div className="hidden lg:block text-4xl font-mono font-extrabold">
-          {keyChar}
-        </div>
-        <div className="font-mono pt-4">{sound}</div>
+    <div className="flex flex-col text-slate-900">
+      <div className="hidden lg:block text-4xl font-mono font-extrabold">
+        {keyChar}
       </div>
-    </button>
+      <div className="font-mono pt-4">{sound}</div>
+    </div>
+    </button> 
   );
 }
 
 export default SoundBtn;
+
+
+
+
+
+{/* <div className="relative group h-24 w-24 lg:h-32 lg:w-32">
+
+<div className="animate-tilt transition group-hover:duration-200 duration-500 group-hover:opacity-100 opacity-75 absolute rounded-lg -inset-0.5 bg-gradient-to-r from-red-900 via-red-700 to-red-500 filter blur"></div>
+<button 
+onClick={handleButtonClick}
+className={`relative h-24 w-24 lg:h-32 lg:w-32 flex items-center justify-center leading-none bg-black rounded-lg focus:outline-none
+transform transition-transform duration-150 ${isPlaying ? "scale-95 " : ""}`}>
+  
+
+
+  <div className="flex flex-col text-gray-300">
+    <div className="hidden lg:block text-4xl font-mono font-extrabold">
+    {keyChar}
+    </div>
+    <div className="font-mono pt-4">{sound}</div>
+  </div>
+</button>
+</div> */}
